eca30e0b8d2d98bb0d9d53d3906576d9a4c31d2f
Remco Dijkman <rmdijkman@gmail.com>
2024-06-22 11:24:00 +0200

    Representing capacity as a triangle.

--- sandbox/pinpress.py
+++ sandbox/pinpress.py
@@ -1,9 +1,62 @@
-import random
-from simpn.simulator import SimProblem, SimToken
+from simpn.simulator import SimProblem, SimToken, SimVar
 import simpn.prototypes as prototype
-from simpn.visualisation import Visualisation
+from simpn.visualisation import Visualisation, Node, TUE_BLUE, TUE_RED, LINE_WIDTH, TEXT_SIZE, TUE_LIGHTBLUE
+import pygame
 
 
+# This class is just here to visualize the capacity place as a triangle.
+# I think it is a bit superfluous, so you can also simply remove it.
+class Capacity(SimVar):
+    def __init__(self, model, _id, priority=lambda token: token.time):
+        """
+        A SimVar that represents a queue in a queueing system.
+        It is just a SimVar with a different visualisation.
+        """
+        super().__init__(_id, priority)
+
+        model.add_prototype_var(self)
+
+    class CapacityViz(Node):
+        def __init__(self, model_node):
+            super().__init__(model_node)
+        
+        def draw(self, screen):
+            x, y = self._pos
+            hw, hh = self._half_width, self._half_height
+            pygame.draw.polygon(screen, TUE_LIGHTBLUE, ((x-hw, y+hh), (x, y-hh), (x+hw, y+hh)))
+            pygame.draw.polygon(screen, TUE_BLUE, ((x-hw, y+hh), (x, y-hh), (x+hw, y+hh)), LINE_WIDTH)
+
+            font = pygame.font.SysFont('Calibri', TEXT_SIZE)
+            bold_font = pygame.font.SysFont('Calibri', TEXT_SIZE, bold=True)
+            
+            # draw label
+            label = font.render(self._model_node.get_id(), True, TUE_BLUE)
+            text_x_pos = self._pos[0] - int(label.get_width()/2)
+            text_y_pos = self._pos[1] + self._half_height + LINE_WIDTH
+            screen.blit(label, (text_x_pos, text_y_pos))
+
+            # draw marking
+            mstr = "["
+            ti = 0
+            for token in self._model_node.marking:
+                mstr += str(token.value) + "@" + str(round(token.time, 2))
+                if ti < len(self._model_node.marking) - 1:
+                    mstr += ", "
+                ti += 1
+            mstr += "]"
+            label = bold_font.render(mstr, True, TUE_RED)
+            text_x_pos = self._pos[0] - int(label.get_width()/2)
+            text_y_pos = self._pos[1] + self._half_height + LINE_WIDTH + int(label.get_height())
+            screen.blit(label, (text_x_pos, text_y_pos))        
+
+    def get_visualisation(self):
+        return self.CapacityViz(self)
+
+
+
+
+
+# this is where the actual example starts
 my_problem = SimProblem()
 
 pinpress = my_problem.add_var("pinpress")
@@ -12,7 +65,8 @@ pinpress.put("pinpress")
 operator = my_problem.add_var("operator")
 operator.put("operator")
 
-pinpress_capacity = my_problem.add_var("pinpress_capacity")
+# pinpress_capacity = my_problem.add_var("pinpress_capacity")
+pinpress_capacity = Capacity(my_problem, "pinpress_capacity")  # this line can also be replaced by the line above to simply represent the capacity as a place
 pinpress_capacity.put(1)
 
 flow_to_transition_in = prototype.BPMNFlow(my_problem, "to_transition_in")
