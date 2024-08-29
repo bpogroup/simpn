471a5e1a41e6a0178789691609f8b55eeb16469a
Remco Dijkman <rmdijkman@gmail.com>
2024-08-29 18:03:03 +0200

    non-determinism made more pronounced.

--- examples/demo/6 nondeterminism.py
+++ examples/demo/6 nondeterminism.py
@@ -1,21 +1,26 @@
 from simpn.simulator import SimProblem, SimToken
 from simpn.visualisation import Visualisation
 
-shop = SimProblem()
+bank = SimProblem()
 
-resources = shop.add_place("resources")
-customers = shop.add_place("customers")
-busy = shop.add_place("busy")
-completed = shop.add_place("completed")
+resources = bank.add_place("employees")
+customers = bank.add_place("customers")
+busy = bank.add_place("busy")
+completed = bank.add_place("completed")
 
-shop.add_transition([customers, resources], [busy], lambda c, r: [SimToken((c, r), delay=0.75 if r=="cassier1" else 1.25)], "start_processing")
-shop.add_transition([busy], [completed, resources], lambda b: [SimToken(b[0]), SimToken(b[1])], "end_processing")
+bank.add_transition([customers, resources], [busy], lambda c, r: [SimToken((c, r), delay=0.75 if r[-1]==c[-1] else 1.25)], "start_processing")
+bank.add_transition([busy], [completed, resources], lambda b: [SimToken(b[0]), SimToken(b[1])], "end_processing")
 
-resources.put("cassier1")
-resources.put("cassier2")
+resources.put("type 1")
+resources.put("type 2")
 for i in range(4):
-    customers.put(f"c{i}")
+    customers.put("type " + str(i%2+1))
 
-Vis = Visualisation(shop, "./temp/nondeterminism_layout.txt")
+Vis = Visualisation(bank, "./temp/nondeterminism_layout.txt")
 Vis.show()
 Vis.save_layout("./temp/nondeterminism_layout.txt")
+
+# In this case, we can add a simple guard to ensure that the customer is processed by the right employee.
+# However, if the relation is much more complicated, or even depends on the state of the system,
+# that becomes infeasible.
+# As an example, consider 
