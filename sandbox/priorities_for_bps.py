34de27557bd3c8191a81e9a5993e49e31257a71d
Dr Adam Banham <adam.banham@outlook.com.au>
2025-11-13 10:49:19 +1030

    chore: added sandbox for testing

---
+++ sandbox/priorities_for_bps.py
@@ -0,0 +1,219 @@
+from simpn.helpers import BPMN
+from simpn.simulator import SimProblem, SimToken, SimTokenValue
+from simpn.visualisation import Visualisation
+
+import random
+from os.path import join, exists
+from enum import Enum, auto
+
+LAYOUT_FILE = join(".", "temp", "priorities_for_bps.layout")
+
+
+class CustomerType(Enum):
+    NEW = auto()
+    BRONZE = auto()
+    SILVER = auto()
+    GOLD = auto()
+
+    @staticmethod
+    def pick_type():
+        r = random.random()
+        if r < 0.5:
+            return CustomerType.NEW
+        elif r < 0.8:
+            return CustomerType.BRONZE
+        elif r < 0.95:
+            return CustomerType.SILVER
+        else:
+            return CustomerType.GOLD
+
+    @staticmethod
+    def convert_new():
+        r = random.random()
+        if r < 0.6:
+            return CustomerType.BRONZE
+        elif r < 0.9:
+            return CustomerType.SILVER
+        else:
+            return CustomerType.GOLD
+
+    def __str__(self):
+        return self.name.lower()
+
+    def __repr__(self):
+        return self.__str__()
+
+
+def employee_speed(base_time, employee: SimTokenValue):
+    return random.expovariate(1 / base_time) / employee.work_speed
+
+
+model = SimProblem()
+
+
+class EmployeePool(BPMN):
+    model = model
+    name = "employees"
+    type = "resource-pool"
+    amount = 0
+
+
+employees = model.var("employees")
+for i in range(5):
+    employees.put(
+        SimTokenValue("employee-{i}".format(i=i + 1), work_speed=random.uniform(2, 5))
+    )
+employees.set_invisible_edges()
+
+
+class Start(BPMN):
+    model = model
+    name = "started-process"
+    type = "start"
+    outgoing = ["considering"]
+
+    def interarrival_time():
+        return random.expovariate(0.8)
+
+    def behaviour(identifier: str):
+        val = SimTokenValue(identifier)
+        val.type = CustomerType.pick_type()
+        return SimToken(val)
+
+
+class Investigate(BPMN):
+    model = model
+    name = "Investigate"
+    type = "task"
+    incoming = ["considering", "employees"]
+    outgoing = ["deciding", "employees"]
+
+    def behaviour(case: SimTokenValue, employee: SimTokenValue):
+        work_time = employee_speed(15, employee)
+
+        new_case = case.clone()
+        if new_case.type == CustomerType.NEW:
+            new_case.type = CustomerType.convert_new()
+            work_time += employee_speed(5, employee)
+        new_case.started = model.var("time").marking[0].value
+
+        return [SimToken((new_case, employee), delay=work_time)]
+
+
+class DecisionXorSplit(BPMN):
+    model = model
+    type = "gat-ex-split"
+    incoming = ["deciding"]
+    outgoing = [
+        "handling-bronze",
+        "handling-silver",
+        "handling-gold",
+    ]
+    name = "What type of customer?"
+
+    def choice(val: SimTokenValue):
+        if val.type == CustomerType.BRONZE:
+            return [SimToken(val), None, None]
+        elif val.type == CustomerType.SILVER:
+            return [None, SimToken(val), None]
+        elif val.type == CustomerType.GOLD:
+            return [None, None, SimToken(val)]
+        else:
+            raise ValueError("Unknown customer type: {}".format(val.type))
+
+
+class InvoiceBronze(BPMN):
+    model = model
+    type = "task"
+    incoming = ["handling-bronze", "employees"]
+    outgoing = ["invoiced-bronze", "employees"]
+    name = "Invoice Bronze"
+
+    def behaviour(val, res):
+        work_time = employee_speed(10, res)
+        return [SimToken((val, res), delay=work_time)]
+
+
+class PickPackageForSilver(BPMN):
+    model = model
+    type = "task"
+    incoming = ["handling-silver", "employees"]
+    outgoing = ["picked-silver", "employees"]
+    name = "Pick Package for Silver"
+
+    def behaviour(val, res):
+        work_time = employee_speed(10, res)
+        return [SimToken((val, res), delay=work_time)]
+
+
+class InvoiceSilver(BPMN):
+    model = model
+    type = "task"
+    incoming = ["picked-silver", "employees"]
+    outgoing = ["invoiced-silver", "employees"]
+    name = "Invoice Silver"
+
+    def behaviour(val, res):
+        work_time = employee_speed(5, res)
+        return [SimToken((val, res), delay=work_time)]
+
+
+class CallGoldCustomer(BPMN):
+    model = model
+    type = "task"
+    incoming = ["handling-gold", "employees"]
+    outgoing = ["called-customer", "employees"]
+    name = "Calling Customer"
+
+    def behaviour(val, res):
+        attempts = random.randint(1, 5)
+        work_time = attempts * employee_speed(2, res)
+        return [SimToken((val, res), delay=work_time)]
+
+
+class PickPackageGold(BPMN):
+    model = model
+    type = "task"
+    incoming = ["called-customer", "employees"]
+    outgoing = ["picked-gold", "employees"]
+    name = "Pick Package for Gold"
+
+    def behaviour(val, res):
+        work_time = employee_speed(20, res)
+        return [SimToken((val, res), delay=work_time)]
+
+
+class InvoiceGold(BPMN):
+    model = model
+    type = "task"
+    incoming = ["picked-gold", "employees"]
+    outgoing = ["invoiced-gold", "employees"]
+    name = "Invoice Gold"
+
+    def behaviour(val, res):
+        work_time = employee_speed(10, res)
+        return [SimToken((val, res), delay=work_time)]
+
+
+class InvoiceXorJoin(BPMN):
+    model = model
+    type = "gat-ex-join"
+    incoming = ["invoiced-gold", "invoiced-silver", "invoiced-bronze"]
+    outgoing = ["invoiced-customer"]
+    name = "Customer Join"
+
+
+class HandledCustomer(BPMN):
+    model = model
+    type = "end"
+    incoming = ["invoiced-customer"]
+    name = "Handled Customer"
+
+
+if exists(LAYOUT_FILE):
+    vis = Visualisation(model, layout_file=LAYOUT_FILE)
+else:
+    vis = Visualisation(model)
+
+vis.show()
+vis.save_layout(join(".", "temp", "priorities_for_bps.layout"))
